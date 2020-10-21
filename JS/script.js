59135c75bd806e5b5e4553f7f48dc857a7c706cd
Abdulaziz Ali <rediabdulaziz@gmail.com>
2020-10-22 00:59:37 +0300

    add book to the array

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,7 @@
-let myLibrary = [];
-const root = document.getElementById('root')
-
+let bookLibrary = [];
+const root = document.getElementById('root');
+const error = document.getElementById('error');
+const formContainer = document.querySelector('.form-container');
 
 function Book(title, author, description,nop) {
   this.title = title;
@@ -10,7 +11,7 @@ function Book(title, author, description,nop) {
 }
 
 function displayBooks() {
-  myLibrary.forEach((book) => {
+  bookLibrary.forEach((book) => {
       const bookCard = document.createElement('div');
       bookCard.innerText = book;
       document.getElementById('root').appendChild(bookCard)
@@ -18,15 +19,37 @@ function displayBooks() {
 }
 
 function bookForm (){
-  formContainer = document.querySelector('.form-container');
   formContainer.style.display = 'block'
 }
 
-document.getElementById('add').addEventListener('click',bookForm);
-
 displayBooks();
 
+validated = (titleField, authorField, nopField, descField) => {
 
 
-function addBookToLibrary(
-) {}
+  if (titleField == '' || authorField == '' || nopField == '' || descField == '') {
+    error.innerText = 'Please enter valid data'
+    return false;
+  }
+
+  return true
+
+}
+
+addBookToLibrary = event => {
+  const title = document.getElementById('titleField').value;
+  const author = document.getElementById('authorField').value;
+  const nop = document.getElementById('nopField').value;
+  const desc = document.getElementById('descField').value;
+
+  if (!validated(title, author, nop, desc)) {
+    return;
+  }
+
+  bookLibrary.push(new Book(title, author, desc, nop))
+  formContainer.style.display = 'none'
+  displayBooks();
+}
+
+document.getElementById('add').addEventListener('click',bookForm);
+document.getElementById('saveBook').addEventListener('click', addBookToLibrary);
